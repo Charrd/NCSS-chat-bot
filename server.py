7d233bfaf4713210ef8e53a31d5cf0be554ad332
SIT NCSS <sit_ncss@shared.sydney.edu.au>
2019-01-12 10:17:03 +1100

    Added server, flaskapp and altered procfile to handle both alexa and slack

---
+++ server.py
@@ -0,0 +1,5 @@
+from flaskapp import app
+import slack, alexa
+
+if __name__ == '__main__':
+  app.run(debug = True)
--- slack.py
+++ slack.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 from bot import on_enter_state, on_input
-
-app = Flask(__name__)
+from flaskapp import app
 
 state = 'NO QUERY'
 context = {}
