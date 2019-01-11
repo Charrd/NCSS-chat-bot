188746b7dc47bda381eeb7b7349a6ec80261798a
Emma PIlgrim <epil8093@SHARED.SYDNEY.EDU.AU>
2019-01-12 09:50:39 +1100

    json uploading

--- slack.py
+++ slack.py
@@ -18,10 +18,15 @@ def slack_event():
     while state != "END":
       state, context, output1 = on_input(state, user_input, context)
       output2 = on_enter_state(state, context)
-      if output1 != None:
-        output += output1
-      if output2 != None:
-        output += output2
+      if type(output2) is dict:
+        output = output2
+
+      elif type(output2) is str or output2 == None:
+        output = ''
+        if output1 != None:
+          output += output1
+        if output2 != None:
+          output += output2
       return jsonify(output)
   return ""
 if __name__ == '__main__':
