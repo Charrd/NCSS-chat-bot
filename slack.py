45417b824ff8a6ff832ecfdfe15659fee08a9acc
Emma PIlgrim <epil8093@SHARED.SYDNEY.EDU.AU>
2019-01-12 11:05:16 +1100

    Return text string

--- slack.py
+++ slack.py
@@ -18,13 +18,15 @@ def slack_event():
     # todo: finish this slack interface!
     while state != "END":
       if text:
+        print('Type: text')
         state, context, output1 = on_input(state, text, context)
       elif payload:
-         json_payload = json.loads(payload)
-         actions = json_payload.get('actions')
-         first_action = actions[0]
-         value = first_action.get("value")
-         state, context, output1 = on_input(state, value, context)
+        print('Type: payload')
+        json_payload = json.loads(payload)
+        actions = json_payload.get('actions')
+        first_action = actions[0]
+        value = first_action.get("value")
+        state, context, output1 = on_input(state, value, context)
       output2 = on_enter_state(state, context)
       if type(output2) is dict:
         output = output2
@@ -35,7 +37,10 @@ def slack_event():
           output += output1
         if output2 != None:
           output += output2
-      return jsonify(output)
+      return jsonify({
+        "response_type": "in_channel",
+        'text': output
+      })
   return ""
 if __name__ == '__main__':
   app.run(debug = True)
