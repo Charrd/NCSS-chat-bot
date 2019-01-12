6bc8dbad69060643a63649bac6bbec6e58a2b7cf
Emma PIlgrim <epil8093@SHARED.SYDNEY.EDU.AU>
2019-01-12 11:31:57 +1100

    I did it wrong the first few times

--- bot.py
+++ bot.py
@@ -70,13 +70,13 @@ def on_input(state, user_input, context):
   elif state == STATE_PLAYLIST:
     return playlist_on_input(user_input, context)
   elif state == STATE_ARTIST:
-    return playlist_on_input(user_input, context)
+    return artist_on_input(user_input, context)
   elif state == STATE_GENRE:
-    return playlist_on_input(user_input, context)
+    return genre_on_input(user_input, context)
   elif state == STATE_SONG:
-    return playlist_on_input(user_input, context)
-  elif state == STATE_INFO:
-    return playlist_on_input(user_input, context)
+    return song_on_input(user_input, context)
+  elif state == STATE_NO_INFO:
+    return IDK_on_input(user_input, context)
 # START STATE
 # The big start state that knows where to send the user.
 # ---
