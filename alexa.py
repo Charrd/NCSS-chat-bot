b85c5156f9e00074783b3702dcf30e37d3a56ce6
SIT NCSS Five <sit_ncss5@SHARED.SYDNEY.EDU.AU>
2019-01-12 20:49:03 +1100

    new bot for alexa bc why not

--- alexa.py
+++ alexa.py
@@ -1,9 +1,46 @@
 from flask import Flask, jsonify, request
-from bot import on_enter_state, on_input
+from bot_alexa import on_enter_state, on_input
 from flaskapp import app
 
 state = 'NO QUERY'
 context = {}
+
+def render_list(response):
+  list_items = []
+  for item in response['list']:
+    list_items.append({
+      'token': item['text'],
+      'textContent': {
+        'primaryText': {
+          'type': 'PlainText',
+          'text': item['text']
+        }
+      },
+    })
+  
+  return {
+    'version': '0.1',
+    'response': {
+      'outputSpeech': {
+        'type': 'SSML',
+        'ssml': f"""<speak><voice name="Russell">{response['text']}</voice></speak>""",
+      },
+      'directives': [
+        {
+          'type': 'Display.RenderTemplate',
+          'template': {
+            'type': 'ListTemplate1',
+            'backButton': 'HIDDEN',
+            'title': response['text'],
+            'token': 'test',
+            'listItems': list_items,
+          }
+        }
+      ],
+      'shouldEndSession': response.get('end', False),
+    }
+  }
+
 
 @app.route('/alexa', methods=['GET', 'POST'])
 def alexa_event():
@@ -21,9 +58,15 @@ def alexa_event():
                 state, context, output1 = on_input(state, user_input, context)
                 output2 = on_enter_state(state, context)['text']
                 if output1 != None:
-                    output += output1 + ' '
+                    if 'list' in output1:
+                        output += jsonify(render_list(output1))
+                    else:
+                        output += output1 + ' '
                 if output2 != None:
-                    output += output2
+                    if 'list' in output1:
+                        output += jsonify(render_list(output2))
+                    else:                    
+                        output += output2
         else:
             output = 'Hi, I\'m Eve, how can i help?'
 
---
+++ bot_alexa.py
@@ -0,0 +1,96 @@
+import re
+import random
+from flask import jsonify
+import json
+import spotify
+import requests
+
+# STATES:
+# None
+# Artist
+# Song
+# Playlist
+# Mood
+
+# ---
+# REGISTER THE STATES
+# Connects our states (eg. 'LOCKED OUT') with our functions (eg. locked_out_on_enter_state)
+# ---
+
+STATE_NO_QUERY = 0
+STATE_MUSIC_CHOICE = 1
+STATE_SONG = 2
+STATE_ARTIST = 3
+STATE_GENRE = 4
+STATE_MOOD = 5
+STATE_PLAYLIST = 6
+STATE_NO_INFO = 7
+STATE_PARTY_TYPE = 8
+
+# What to do when we enter a state
+def on_enter_state(state, context):
+  if state == STATE_NO_QUERY:
+    return no_query_on_enter_state(context)
+  # start of music bot
+  elif state == STATE_MUSIC_CHOICE:
+    return music_choice_on_enter_state(context)
+
+  # More states here
+  # elif state == ...
+  raise NotImplementedError(f"Invalid state for on enter {state}")
+
+# What to do when we receive input while in a state
+def on_input(state, user_input, context):
+  # First up, if they're trying to quit, then quit.
+  if user_input == 'quit':
+    return STATE_NO_QUERY, {}, 'Bye! \n'
+
+  # Otherwise, check the state.
+  if state == STATE_NO_QUERY:
+    return no_query_on_input(user_input, context)
+  
+  #start of music bot
+  elif state == STATE_MUSIC_CHOICE:
+    return music_choice_on_input(user_input, context)
+
+
+  raise NotImplementedError(f"Invalid state for on input {state}")
+# START STATE
+# The big start state that knows where to send the user.
+# ---
+
+def no_query_on_enter_state(context):
+  return 'Hi I\'m Eve how can I help? '
+
+def no_query_on_input(user_input, context):
+  search = re.search('music', user_input)
+  if search:
+    return STATE_MUSIC_CHOICE, {}, None
+
+
+  # If we didn't match any regex, go back to this start state and try again.
+  else:
+    return STATE_NO_QUERY, {}, 'Sorry, I don\'t understand!'
+
+
+# ------------
+# OTHER STATES
+# ------------
+
+
+
+
+# --- More states go here! --- #
+
+def music_choice_on_enter_state(context):
+  return {
+    "text": "What song do you want? "
+    }
+
+def music_choice_on_input(user_input, context):
+  song = requests.get(spotify.track_by_name(user_input))
+
+  headers = {'Authorization': 'Bearer xoxb-498969795956-521435106288-iXazpPMO1WCj08WEoWVwCAHH'}
+
+  response = requests.post('https://api.slack.com/api/files.upload', files={'file': song.content}, headers=headers, data={'channels': '#general', 'filetype': 'mp3'})
+  return STATE_NO_QUERY, {} , 'thanks for selecting'
