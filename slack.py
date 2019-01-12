826c645d1f752867d46e084d00e3bb83451077cf
Emma PIlgrim <epil8093@SHARED.SYDNEY.EDU.AU>
2019-01-12 11:12:13 +1100

    fixed jsonify output

--- slack.py
+++ slack.py
@@ -29,18 +29,21 @@ def slack_event():
         state, context, output1 = on_input(state, value, context)
       output2 = on_enter_state(state, context)
       if type(output2) is dict:
+        print("return Type: dict")
         output = output2
+        return jsonify(output)
 
       elif type(output2) is str or output2 == None:
+        print("return Type: string")
         output = ''
         if output1 != None:
           output += output1
         if output2 != None:
           output += output2
-      return jsonify({
-        "response_type": "in_channel",
-        'text': output
-      })
+        return jsonify({
+          "response_type": "in_channel",
+          'text': output
+        })
   return ""
 if __name__ == '__main__':
   app.run(debug = True)
